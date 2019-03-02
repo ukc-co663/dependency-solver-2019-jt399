cff69255e6d51c428ee8fee219646bc953cb729b
jt399 <46963352+jt399@users.noreply.github.com>
2019-03-02 11:25:00 +0000

    Update Main.java

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -73,7 +73,7 @@ public class Main {
       
       System.out.println(currentCon);
       
-      if(currentCon.equals(A))
+      if(currentCon.equals("A"))
         {
           String CommandOutput = "";
         
