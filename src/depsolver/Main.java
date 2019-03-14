809b972456088d85610a14e0b7e3809d684b55e3
jt399 <46963352+jt399@users.noreply.github.com>
2019-03-14 02:40:52 +0000

    Update Main.java
    Conflict Test

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -106,7 +106,7 @@ public class Main {
         System.out.printf("\n");
       }
       
-      for (List<String> conf : p.getConflicts()) 
+      for (<String> conf : p.getConflicts()) 
 	    {
         System.out.printf("  cons:");
         for (String r : conf) 
