7d7b8bb3a9325168ddd0157567424be22c828729
jt399 <46963352+jt399@users.noreply.github.com>
2019-03-14 18:50:22 +0000

    Update Main.java
    Test

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -137,7 +137,8 @@ public class Main {
       System.out.print(NegativeCon);
       System.out.print(ValidPackages);
 	    
-	    System.out.print(PackageConflicts);
+      //System.out.print(PackageConflicts);
+      System.out.print(ValidPackages.get("A = 2.01"););
     }
   }
 
